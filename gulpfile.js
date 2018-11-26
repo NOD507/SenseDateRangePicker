b679f5659804ea9a374c5e291fe83ad60a5d412e
Philip Olsén <pol@qlik.com>
2018-11-26 13:10:44 +0100

    Added gulp for local build

---
+++ gulpfile.js
@@ -0,0 +1,96 @@
+/* global require process Buffer */
+var gulp = require('gulp');
+var cssnano = require('gulp-cssnano');
+var gutil = require('gulp-util');
+var uglify = require('gulp-uglify');
+var pkg = require('./package.json');
+
+var DIST = './dist',
+	SRC = './src',
+	NAME = pkg.name;
+
+gulp.task('qext', function () {
+	var qext = {
+		name: 'Date picker',
+		type: 'visualization',
+		description: pkg.description,
+		version: pkg.version,
+		icon: 'calendar',
+		preview: 'preview.png',
+		keywords: 'qlik-sense, visualization',
+		author: pkg.author,
+		homepage: pkg.homepage,
+		license: pkg.license,
+		repository: pkg.repository,
+		installer: 'QlikExtensionBundler',
+		bundle: {
+			id: 'qlik-dashboard-bundle',
+			name: 'Qlik Dashboard bundle',
+			description: "This is a set of supported extensions that will facilitate dashboard creation in Qlik Sense: A navigation button, a date picker, a slider, and two different container objects. These can be used in addition to the native objects found under 'Charts'.\n\nFor limitations and support conditions, see the documentation."
+		},
+		dependencies: {
+			'qlik-sense': '>=5.5.x'
+		}
+	};
+	if (pkg.contributors) {
+		qext.contributors = pkg.contributors;
+	}
+	var src = require('stream').Readable({
+		objectMode: true
+	});
+	src._read = function () {
+		this.push(new gutil.File({
+			cwd: '',
+			base: '',
+			path: NAME + '.qext',
+			contents: Buffer.from(JSON.stringify(qext, null, 4))
+		}));
+		this.push(null);
+	};
+	return src.pipe(gulp.dest(DIST));
+});
+
+gulp.task('clean', function (ready) {
+	var del = require('del');
+	del.sync([DIST]);
+	ready();
+});
+
+gulp.task('css', function (){
+    return gulp.src(SRC + '/**/*.css')
+        .pipe(cssnano())
+        .pipe(gulp.dest(DIST));
+});
+
+gulp.task('build',['clean', 'qext', 'css'], function () {
+    gulp.src([
+        SRC + '/**/*.png',
+        SRC + '/**/*.txt'
+    ])
+		.pipe(gulp.dest(DIST));
+
+	return gulp.src(SRC + '/**/*.js')
+		.pipe(uglify({mangle:false}))
+		.pipe(gulp.dest(DIST));
+});
+
+gulp.task('zip', ['build'], function () {
+	var zip = require('gulp-zip');
+
+	return gulp.src(DIST + '/**/*')
+		.pipe(zip(`${NAME}_${pkg.version}.zip`))
+		.pipe(gulp.dest(DIST));
+});
+
+gulp.task('debug', ['clean', 'qext'], function () {
+	return gulp.src([
+		SRC + '/**/*.png', 
+        SRC + '/**/*.js',
+        SRC + '/**/*.css',
+        SRC + '/**/*.txt',
+		DIST + '/**/*.qext'
+	])
+		.pipe(gulp.dest(DIST));
+});
+
+gulp.task('default', ['build']);
