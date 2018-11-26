cc9f7c1918abbb6e80cf7be55b94315a6ecb564a
Philip Olsén <pol@qlik.com>
2018-11-26 13:35:01 +0100

    Add config for circleci

---
+++ .circleci/get-package-version.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -o errexit
+
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+
+echo $PACKAGE_VERSION
